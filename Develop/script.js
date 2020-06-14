bf391032070ab05ab20a30b2a3ce9b2279ef5085
Joey Ramos <jramos1202@gmail.com>
2020-06-14 18:04:56 -0500

    set up variables in javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,12 @@
 // Assignment code here
+var characterAmountRange = document.getElementById("characterAmountRange")
+var characterAmountNumber = document.getElementById("characterAmountNumber")
+var includeUpperCase = document.getElementById("includeUpperCase")
+var includeLowerCase = document.getElementById("includeLowerCase")
+var includeNumbers = document.getElementById("includeNumbers")
+var includeSymbols = document.getElementById("includeSymbols")
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,5 +20,19 @@ function writePassword() {
 
 }
 
+// Sync character amount slider and number box
+function syncCharacterAmount(event) {
+  var value = event.target.value
+  characterAmountRange.value = value
+  characterAmountNumber.value = value
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Add event listner to input character range
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
